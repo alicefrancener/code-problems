ad7e6163ff002dc2036b4a73e198740c32872c5d
Alice Francener <alicefrancener@gmail.com>
2020-08-04 11:31:38 -0300

    uri(beginner): Average 2

--- uri/beginner/1006.c
+++ uri/beginner/1006.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
+/*
+  author: Alice Francener
+  problem: Average 2
+  url: https://www.urionlinejudge.com.br/judge/en/problems/view/1006
+*/
 
-int main(){
-	double a,b,c,media;
-	scanf("%lf %lf %lf",&a,&b,&c);
-	media=(2.0*a+3.0*b+5.0*c)/10.0;
-	printf("MEDIA = %.1lf\n",media);
+#include <stdio.h>
+
+int main()
+{
+	double a, b, c, media;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	media = (2.0 * a + 3.0 * b + 5.0 * c) / 10.0;
+	printf("MEDIA = %.1lf\n", media);
 	return 0;
 }
