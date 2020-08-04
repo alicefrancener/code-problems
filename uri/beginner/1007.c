d23df461d7c7dc98622d70d1941600118c79e265
Alice Francener <alicefrancener@gmail.com>
2020-08-04 11:33:34 -0300

    uri(beginner): Difference

--- uri/beginner/1007.c
+++ uri/beginner/1007.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
+/*
+  author: Alice Francener
+  problem: Difference
+  url: https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+*/
 
-int main(){
-	int a,b,c,d,dif;
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	dif=(a*b-c*d);
-	printf("DIFERENCA = %d\n",dif);
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, d, dif;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	dif = (a * b - c * d);
+	printf("DIFERENCA = %d\n", dif);
 	return 0;
 }
