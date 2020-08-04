34d7d1758c8dd65aded63580f6dee0797bddf852
Alice Francener <alicefrancener@gmail.com>
2020-08-04 11:34:33 -0300

    uri(beginner): Salary

--- uri/beginner/1008.c
+++ uri/beginner/1008.c
@@ -1,10 +1,17 @@
-#include<stdio.h>
+/*
+  author: Alice Francener
+  problem: Salary
+  url: https://www.urionlinejudge.com.br/judge/en/problems/view/1008
+*/
 
-int main(){
-	int n,horas;
-	float valorhora,salario;
-	scanf("%d %d %f",&n,&horas,&valorhora);
-	salario=horas*valorhora;
-	printf("NUMBER = %d\nSALARY = U$ %.2f\n",n,salario);
+#include <stdio.h>
+
+int main()
+{
+	int n, horas;
+	float valorhora, salario;
+	scanf("%d %d %f", &n, &horas, &valorhora);
+	salario = horas * valorhora;
+	printf("NUMBER = %d\nSALARY = U$ %.2f\n", n, salario);
 	return 0;
 }
